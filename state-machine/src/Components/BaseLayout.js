2f9f1a2c1f07ba783627a340fbcffa1062363e16
LAPTOP-VUQ1J6NP\Victor_Moreno <victorsahir1011@gmail.com>
2023-12-01 00:31:25 -0500

    Conociendo las propiedades del objecto de la máquina de estado.

--- state-machine/src/Components/BaseLayout.js
+++ state-machine/src/Components/BaseLayout.js
@@ -5,6 +5,11 @@ import bookingMachine from '../Machines/bookingMachine';
 const BaseLayout = () => {
     const [state, send] = useMachine(bookingMachine);
     
+    console.log('nuestra maquina', state);
+    console.log('matches true', state.matches('initial')); // Evalua si la máquina esta en el pasado como parámetro.
+    console.log('matches false', state.matches('tickets'));
+    console.log('can', state.can('FINISH')); // Evalua si se puede ejecutar el evento.
+    
     return (
         <div>Hola</div>
     );
