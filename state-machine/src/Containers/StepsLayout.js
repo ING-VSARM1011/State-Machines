f6c7018b8e3965b818a41f3b6e58da1e3852179b
LAPTOP-VUQ1J6NP\Victor_Moreno <victorsahir1011@gmail.com>
2023-12-01 02:43:44 -0500

    Implementando servicio para la consulta de la lista de paises.

--- state-machine/src/Containers/StepsLayout.js
+++ state-machine/src/Containers/StepsLayout.js
@@ -9,7 +9,7 @@ export const StepsLayout = ({ state, send }) => {
 
   const renderContent = () => {
 
-    if (state.matches("search")) return <Search send={send} />;
+    if (state.matches("search")) return <Search state={state} send={send} />;
     if (state.matches("initial")) return <Welcome send={send} />;
     if (state.matches("passengers")) return <Passengers state={state} send={send} />;
     if (state.matches("tickets")) return <Tickets send={send} />;
